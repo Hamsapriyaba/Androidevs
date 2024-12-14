fe4427fe3951000156497baf03e2de823bad0466
Hamsapriya B A <140936684+Hamsapriyaba@users.noreply.github.com>
2024-12-14 07:21:57 +0530

    Update signin.java

--- signin/signin.java
+++ signin/signin.java
@@ -1,46 +1,53 @@
 package com.example.signup;
-import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
 import android.content.Intent;
+import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-public class Signin extends AppCompatActivity {
-   EditText eusername, epwd;
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class login extends AppCompatActivity {
+    EditText euername,epwd;
     Button login;
     int count=0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_signin);
-        eusername = (EditText) findViewById(R.id.username);
-        epwd = (EditText)findViewById(R.id.password);
-        login = (Button)findViewById(R.id.login);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_login);
+        euername=(EditText) findViewById(R.id.loginuser);
+        epwd=(EditText) findViewById(R.id.loginpswd);
+        login=(Button) findViewById(R.id.login);
         String regemail=getIntent().getStringExtra("email");
         String regpwd=getIntent().getStringExtra("userpwd");
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String newemail=eusername.getText().toString();
+                String newemail=euername.getText().toString();
                 String newpwd=epwd.getText().toString();
                 if(regemail.equals(newemail) && regpwd.equals(newpwd)){
-                    Toast.makeText(Signin.this, "Login Successful", Toast.LENGTH_LONG).show();
-                    Intent intent=new Intent(Signin.this,Welcome.class);
+                    Toast.makeText(login.this,"Login Successful",Toast.LENGTH_LONG).show();
+                    Intent intent=new Intent(login.this, homepage.class);
                     startActivity(intent);
-                    eusername.setText("");
+                    euername.setText("");
                     epwd.setText("");
                 }
                 else{
                     count++;
-                    Toast.makeText(Signin.this, "Login Failed "+count, Toast.LENGTH_LONG).show();
-                    if(count==3)
-                    {
+                    Toast.makeText(login.this,"Login Failed "+count,Toast.LENGTH_LONG).show();
+                    if(count==3){
                         login.setEnabled(false);
-                        Toast.makeText(Signin.this, "Login Failed "+count, Toast.LENGTH_LONG).show();
+                        Toast.makeText(login.this,"Login Failed "+count,Toast.LENGTH_LONG).show();
                     }
                 }
-             }); 
-}
+            }
+        });
+    }
 }
